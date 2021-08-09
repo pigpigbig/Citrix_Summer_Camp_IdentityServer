9844a1ddf2426924a123ef497cae3eeec0edc820
finger1517 <finger1517@163.com>
2021-08-09 14:05:12 +0800

    first commit

---
+++ Yuan.IdentityServer4/TestUsers.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Yuan.IdentityServer4
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "1",
+                        Username = "alice",
+                        Password = "alice",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "alice@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://baidu.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json),
+                           
+                            new Claim(JwtClaimTypes.Role,"admin")  //添加角色
+                        },
+
+                    }
+                };
+            }
+        }
+    }
+}
